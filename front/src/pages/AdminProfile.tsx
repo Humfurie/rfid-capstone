591252d4c71fc954495df897ab36633755809adb
Humfurie <humfurie@gmail.com>
2023-01-19 18:46:59 +0800

    deleting head tags in AdminProfile
    Signed-off-by: Humfurie <humfurie@gmail.com>

--- front/src/pages/AdminProfile.tsx
+++ front/src/pages/AdminProfile.tsx
@@ -1,17 +1,13 @@
-import Head from "next/head";
+import { useState } from "react";
 import AdminNavbar from "../components/AdminComponents/AdminNavbar";
 import AdminProfileContent from "../components/AdminComponents/AdminProfileContent";
 import Header from "../components/Header";
 
 
 const AdminNavbarProfile = () => {
+	const [form, setForm] = useState()
     return (
         <div >
-			<Head>
-				<title>Admin's Profile</title>
-				<meta name="description" content="Created by streamline" />
-				<link rel="icon" href=".../img/ais-rft-logo.jpg" />
-			</Head>
 			<div>
 				<Header />
 				<div className="inline-flex">
