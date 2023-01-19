804416a474aff77e4e1a51752c5d7d3e77a4918d
clofelmaeee <campuganmaec@gmail.com>
2023-01-19 18:54:54 +0800

    deleting head cause it's not useful
    Signed-off-by: clofelmaeee <campuganmaec@gmail.com>

--- front/src/pages/RecordsEmployeesDashboard.tsx
+++ front/src/pages/RecordsEmployeesDashboard.tsx
@@ -1,4 +1,3 @@
-import  Head  from "next/head";
 import AdminNavbar from "../components/AdminComponents/AdminNavbar";
 import Header from "../components/Header";
 import RecordsEmployeesContent from "../components/UsersComponents/RecordsEmployeesContent";
@@ -6,11 +5,6 @@ import RecordsEmployeesContent from "../components/UsersComponents/RecordsEmploy
 const RecordsEmployeesDashboard = () => {
     return (
         <div>
-            <Head>
-				<title>Admin's Dashboard</title>
-				<meta name="description" content="Created by streamline" />
-				<link rel="icon" href=".../img/ais-rft-logo.jpg" />
-			</Head>
             <div>
                 <Header/>
             </div>
