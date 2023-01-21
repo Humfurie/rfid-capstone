d2aaca7176c1207f9ebae60fbb9d20bca1ba14b1
Humphrey Singculan <Humfurie@gmail.com>
2023-01-21 19:27:27 +0800

    parent state and form management
    Signed-off-by: Humphrey Singculan <Humfurie@gmail.com>

--- front/src/components/UsersComponents/UsersParentsContent.tsx
+++ front/src/components/UsersComponents/UsersParentsContent.tsx
@@ -1,28 +1,30 @@
-import { useState } from "react";
+import { useContext } from "react";
+import { FormContext } from "../../lib/FormContext";
 import MyButton from "../../lib/partials/MyButton";
-import MyInput from "../../lib/partials/MyInput";
-import UsersRegistrationForm from "../UsersRegistrationForm";
+import ParentRegistration from "../UsersRegistrationComponents/ParentRegistration";
 import SearchBar from "./SearchBar";
 
 const UsersParentsContent = () => {
-    const [registration, setRegistration] = useState(false)
-    return (
-        <div className="content">
-            <div className="top-status-content ml-6 mt-6 inline-flex">
-                <MyButton type='button' label='Add Parent' onClick={(e: any) => {
-                    setRegistration(true)
-                }}
-                    className="text-black bg-powderblue-shades10% hover:bg-powderblue-shades20%  font-medium rounded-lg text-sm px-4 py-2 "
-                />
-                <UsersRegistrationForm registration={registration} setRegistration={setRegistration} />
-                <div>
-                    <SearchBar />
-                </div>
 
-            </div>
+    const { setRegistration} = useContext(FormContext)
+  return (
+    <div className="content">
+      <div className="top-status-content ml-6 mt-6 inline-flex">
+        <MyButton
+          type="button"
+          label="Add Parent"
+          onClick={(e: any) => {
+            setRegistration(true);
+          }}
+          className="text-black bg-powderblue-shades10% hover:bg-powderblue-shades20%  font-medium rounded-lg text-sm px-4 py-2 "
+        />
+        <ParentRegistration />
+        <div>
+          <SearchBar />
         </div>
-    );
-
-}
+      </div>
+    </div>
+  );
+};
 
 export default UsersParentsContent;
